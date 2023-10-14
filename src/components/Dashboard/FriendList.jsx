763c8114679e163c44c902b90547469d7cf190a6
Sabbir Hossain <sabbir2always@gmail.com>
2023-10-14 12:01:01 +0600

    Search feature fully updated no bug found for the moment

--- src/components/Dashboard/FriendList.jsx
+++ src/components/Dashboard/FriendList.jsx
@@ -6,15 +6,35 @@ import AddFriend from "../AddFriend";
 import { getAuth } from "firebase/auth";
 import defaultProfile from "../../assets/defaultProfile.jpg";
 
-const FriendList = function ({ item, isAdded, requested, adds }) {
-  const [added, setAdded] = useState(false);
+const FriendList = function ({ item, authUid }) {
+  //
+  const [isPending, setIsPending] = useState(false);
+  const [isAdded, setIsAdded] = useState(false);
+
   let profilePic = item.profilePic;
   if (!item.profilePic) {
     profilePic = defaultProfile;
   }
   /// adding friend
   useEffect(() => {
-    setAdded(false);
+    if (!item.friends) {
+      // checking is there any request yet
+      setIsAdded(false);
+      setIsPending(false);
+      return;
+    }
+    const listOfFriend = Object.values(item.friends);
+    // checking if friend already added
+    listOfFriend.find((item) => {
+      if (item.userId === authUid && item.status === "pending") {
+        console.log("pending");
+        setIsPending(true);
+      }
+      if (item.userId === authUid && item.status === "success") {
+        console.log("added");
+        setIsAdded(true);
+      }
+    });
   }, [item]);
 
   const addRequestHandler = function (event) {
@@ -24,19 +44,21 @@ const FriendList = function ({ item, isAdded, requested, adds }) {
     const names = auth.currentUser?.displayName;
 
     AddFriend("add", requireId, currentUser, names);
-    setAdded(true);
+    setIsPending(true);
   };
+
+  // returning when out account comes
+  if (item.uid === authUid) return;
 
   return (
     <div className={classes.friendList}>
       <div className={classes.friendCard}>
         <img src={profilePic} />
         <h3>{item.userName}</h3>
+        {isPending && !isAdded && <p>Pending</p>}
         {isAdded && <p>Added</p>}
 
-        {(requested || added) && <p>Pending</p>}
-
-        {!isAdded && !requested && !added && (
+        {!isPending && !isAdded && (
           <Button onClick={addRequestHandler} id={item.uid}>
             Say Hello
           </Button>
